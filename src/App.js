f0e4398ef8b55d8580c70b869f82740aa4353635
haodz2000 <tahuuhao1810@gmail.com>
2022-04-29 16:22:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {Button} from '@mui/material'
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button variant="text" >Waring</Button>
+        <Button startIcon={<SendRoundedIcon/>} color="success" variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
